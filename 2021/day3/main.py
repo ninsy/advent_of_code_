f6f5cfb144324bbc2e2531773d2aa9a58fd0ef9e
ninsy <kwojciechowski094@gmail.com>
2021-12-06 02:11:35 +0100

    final for day3

--- 2021/day3/main.py
+++ 2021/day3/main.py
@@ -5,47 +5,64 @@ def read_lines(path = "./input.txt"):
         return lines
 
 
-class Solver:
-    def __init__(self, method, line_length):
-        self.method = method
+class Counter:
+    def __init__(self, lines):
         self.counter = []
-        print("init with: {}".format(line_length + 1))
-        for i in range(line_length):
+        for i in range(len(lines[0])):
             self.counter.append([0,0])
 
-    def make_step(self, line):
-        bits = list(line)
-        for idx, bit in enumerate(bits):
-            self.method(self, idx, bit)
+        for _, line in enumerate(lines):
+            bits = list(line)
+            for idx, bit in enumerate(bits):
+                self.counter[idx][int(bit)]+=1
 
 
-def part_1_step(s: Solver, idx, bit):
-    s.counter[idx][int(bit)]+=1
-
-
-def part_2_step(s: Solver, idx, bita):
-    pass
-
-
-def solve(lines, method):
-    s = Solver(method, len(lines[0]))
-    for _, line in enumerate(lines):
-        s.make_step(line)
+def solve_part1(lines):
+    s = Counter(lines)
 
     gamma_bits = []
     epsilon_bits = []
-    for idx, bit_row in enumerate(s.counter):
+    for _, bit_row in enumerate(s.counter):
         zeros, ones = bit_row
 
         gamma_bits.append('0' if zeros > ones else '1')
-        epsilon_bits.append('0' if zeros < ones else '1')\
+        epsilon_bits.append('0' if zeros < ones else '1')
     
     return int(''.join(gamma_bits), 2) * int(''.join(epsilon_bits), 2)
 
 
+def part2_solver(lines, inc_mode):
+    for idx in range(len(lines[0])):
+        s = Counter(lines)
+    
+        zeros, ones = s.counter[idx]
+        if len(lines) == 1:
+            return lines
+        if ones > zeros:
+            # keep ones
+            lines = list(filter(lambda line: line[idx] == '1' if inc_mode else line[idx] == '0', lines))
+        elif zeros > ones:
+            # keep zeros
+            lines = list(filter(lambda line: line[idx] == '0' if inc_mode else line[idx] == '1', lines))
+        elif zeros == ones:
+            # keep with one in that pos
+            for line in lines:
+                if inc_mode and line[idx] == '1':
+                    return [line]
+                elif not inc_mode and line[idx] == '0':
+                    return [line]
+
+
+def solve_part2(all_lines):
+    oxygen_rating = part2_solver(all_lines.copy(), True)[0]
+    co2_scruber_rating = part2_solver(all_lines.copy(), False)[0]
+
+    return int(''.join(oxygen_rating), 2) * int(''.join(co2_scruber_rating), 2)
+
+
 if __name__ == '__main__':
-    result1 = solve(read_lines(), part_1_step)
+    result1 = solve_part1(read_lines())
     print(result1)
 
-    # result2 = solve(read_lines(), part_2_step)
-    # print(result2)
+    result2 = solve_part2(read_lines())
+    print(result2)
